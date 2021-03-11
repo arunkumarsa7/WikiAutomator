2252a383e709ac9e7a0d73ca241300daf0f2eca9
arunkumarsa7 <arunkumarsa8374@gmail.com>
2021-03-11 12:05:34 +0530

    Build test

--- src/com/automate/wiki/service/WikiModifier.java
+++ src/com/automate/wiki/service/WikiModifier.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 
 import com.automate.wiki.helper.ConfigReader;
 import com.automate.wiki.helper.WebDriverVault;
+import com.automate.wiki.helper.WikiAutomatorHelper;
 
 public class WikiModifier {
 
@@ -21,32 +22,32 @@ public class WikiModifier {
 					.waitAndLoadWebElement(By.xpath(ConfigReader.getEditPageEditButton()));
 			editElement.click();
 			final WebDriver webDriver = WebDriverVault.getWebDriver();
-			System.out.println(webDriver.getPageSource());
+//			System.out.println(webDriver.getPageSource());
 //			WebDriverVault.switchToFrame(webDriver.findElement(By.id(ConfigReader.getEditPageIFrameId())));
-//			final WebElement entryElement = WebDriverVault
-//					.waitAndLoadWebElement(By.xpath(ConfigReader.getEntryElementXPath()));
-//			WebDriverVault.getJavascriptExecutor().executeScript(WikiAutomatorHelper.generateLatestWikiEntryForEdit(),
-//					entryElement);
+			final WebElement entryElement = WebDriverVault.waitAndLoadWebElement(
+					By.xpath("//p[contains(text(), '|| Suche in allen Entwicklernews ||')]//following::div[1]"));
+			WebDriverVault.getJavascriptExecutor().executeScript(WikiAutomatorHelper.generateLatestWikiEntryForEdit(),
+					entryElement);
 //			WebDriverVault.switchToDefault();
-//			final WebElement notifyWatchersElement = webDriver
-//					.findElement(By.xpath(ConfigReader.getEditPageNotifyWatchersCheckbox()));
-//			if ((ConfigReader.isNotifyEntwicklerNewsWatchers() && !notifyWatchersElement.isSelected())
-//					|| (!ConfigReader.isNotifyEntwicklerNewsWatchers() && notifyWatchersElement.isSelected())) {
-//				notifyWatchersElement.click();
-//			}
-//			if (ConfigReader.isPreviewEntwicklerNews()) {
-//				final WebElement ellipsisButtonElement = webDriver
-//						.findElement(By.xpath(ConfigReader.getEditPageEllipsisButton()));
-//				ellipsisButtonElement.click();
-//				final WebElement previewButtonElement = webDriver
-//						.findElement(By.xpath(ConfigReader.getEditPagePreviewButton()));
-//				previewButtonElement.click();
-//			}
-//			if (ConfigReader.isPublishEntwicklerNews()) {
-//				final WebElement publishButtonElement = webDriver
-//						.findElement(By.xpath(ConfigReader.getEditPagePublishButton()));
-//				publishButtonElement.click();
-//			}
+			final WebElement notifyWatchersElement = webDriver
+					.findElement(By.xpath(ConfigReader.getEditPageNotifyWatchersCheckbox()));
+			if ((ConfigReader.isNotifyEntwicklerNewsWatchers() && !notifyWatchersElement.isSelected())
+					|| (!ConfigReader.isNotifyEntwicklerNewsWatchers() && notifyWatchersElement.isSelected())) {
+				notifyWatchersElement.click();
+			}
+			if (ConfigReader.isPreviewEntwicklerNews()) {
+				final WebElement ellipsisButtonElement = webDriver
+						.findElement(By.xpath(ConfigReader.getEditPageEllipsisButton()));
+				ellipsisButtonElement.click();
+				final WebElement previewButtonElement = webDriver
+						.findElement(By.xpath(ConfigReader.getEditPagePreviewButton()));
+				previewButtonElement.click();
+			}
+			if (ConfigReader.isPublishEntwicklerNews()) {
+				final WebElement publishButtonElement = webDriver
+						.findElement(By.xpath(ConfigReader.getEditPagePublishButton()));
+				publishButtonElement.click();
+			}
 			System.out.println("Entwicklernews updated successfully");
 		} catch (final NoSuchElementException ne) {
 			System.out.println(ne.getMessage());
