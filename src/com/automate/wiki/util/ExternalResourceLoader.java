349bbb158debfe72b0f74bbc7f55f776e5a71c4a
arunkumarsa7 <arunkumarsa8374@gmail.com>
2021-06-17 21:30:08 +0530

    Latest build

--- src/com/automate/wiki/util/ExternalResourceLoader.java
+++ src/com/automate/wiki/util/ExternalResourceLoader.java
@@ -5,6 +5,7 @@ import static com.automate.wiki.constant.WikiAutomatorConstants.WIKI_AUTOMATOR_P
 import static com.automate.wiki.constant.WikiAutomatorConstants.WIKI_AUTOMATOR_PROPERTY_RESOURCES_LOCATION;
 
 import java.io.File;
+import java.net.URISyntaxException;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -23,8 +24,13 @@ public class ExternalResourceLoader {
 	private static String externalResourceLocation;
 
 	static {
-		jarFileLocation = new File(ClassLoader.getSystemClassLoader().getResource(".").getPath()).getAbsolutePath()
-				.replace("%20", " ");
+		try {
+			final File jarFile = new File(
+					ExternalResourceLoader.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+			jarFileLocation = jarFile.getParent();
+		} catch (final URISyntaxException e) {
+			System.err.println("ERROR : " + e.getMessage());
+		}
 	}
 
 	private static void loadExternalConfigLocation() {
